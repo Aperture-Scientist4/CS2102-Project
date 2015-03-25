0bf8a7dac962694f6d4fc78bac69604b136b69b5
ZHAO HANG <davidzhao-81@163.com>
2015-03-25 20:18:52 +0800

    urls changes

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from store import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.IndexView.as_view(), name='index')
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^restricted/$', views.restricted, name='restricted'),
+    url(r'^restricted/changePassword/$', views.password_change, name='changePassword'),
+    )
