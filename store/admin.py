4dddd3d50ca64132c89fb72e2da3cafc4c69ed8e
ZHAO HANG <davidzhao-81@163.com>
2015-03-25 19:48:39 +0800

    delete the table name in admin

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from store.models import App, Customer, Purchased, Rent
-
-admin.site.register(App)
-admin.site.register(Customer)
-admin.site.register(Purchased)
-admin.site.register(Rent)
