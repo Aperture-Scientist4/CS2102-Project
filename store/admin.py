1ef0fad947c0462e47857f4613ef652dbaa32e55
ZHAO HANG <davidzhao-81@163.com>
2015-03-28 00:39:02 +0800

    register models admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from store.models import Rent, Purchased, App
+from django.contrib.auth.models import User
+
+admin.site.register(Rent)
+admin.site.register(Purchased)
+admin.site.register(App)
+
