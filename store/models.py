be9c4ffdc774792b59d82f7a3b02d5d9aa8fd3bd
ZHAO HANG <davidzhao-81@163.com>
2015-03-28 13:58:29 +0800

    rearrange

--- store/models.py
+++ store/models.py
@@ -19,35 +19,24 @@ class App(models.Model):
 	release_date = models.DateField()
 	genre = models.CharField(max_length=1, choices=GENRE_CHOICES)
 	def __str__(self):
-		return self.appid
-'''
-class Customer(models.Model):
-	email = models.CharField(max_length=128, primary_key=True)
-	password = models.CharField(max_length=128)
-	def __str__(self):
-		return self.email
-	def check_password(self, password):
-		return self.password == password
-	def retrieve_purchased(self):
-		return Purchased.objects.filter(email=self.email)
-	def retrieve_rent(self):
-		return Rent.objects.filter(email=self.email)
-'''
+		return self.name
+
 class Purchased(models.Model):
 	order_id = models.CharField(max_length=32, primary_key=True)
-	username = models.ForeignKey(User)
-	appid = models.ForeignKey(App)
 	rating = models.PositiveIntegerField(null=True)
 	review = models.CharField(max_length=1024, null=True)
+	userid = models.ForeignKey(User)        
+	appid = models.ForeignKey(App)
 	def __str__(self):
 		return self.order_id
 
 class Rent(models.Model):
 	order_id = models.CharField(max_length=32, primary_key=True)
-	username = models.ForeignKey(User)
-	appid = models.ForeignKey(App)
 	rating = models.PositiveIntegerField(null=True)
 	review = models.CharField(max_length=1024, null=True)
 	expire_date = models.DateField()
+	userid = models.ForeignKey(User)
+	appid = models.ForeignKey(App)
 	def __str__(self):
 		return self.order_id
+
