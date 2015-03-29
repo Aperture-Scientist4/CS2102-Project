8b2a9c7253a0be36f9f82ccd024ea391c3e3c318
luckyxkw <cocolonel.x@gmail.com>
2015-03-29 11:55:10 +0800

    Specify some of the static methods

--- store/forms.py
+++ store/forms.py
@@ -9,6 +9,7 @@ class UserForm(forms.ModelForm):
         fields = ('username', 'email', 'password')
 
 class SearchForm(forms.Form):
+    @staticmethod
     def get_choices():
         return [('Cats','Cats'),('Dogs','Dogs')]
     def __init__(self,*args,**kwargs):
@@ -17,6 +18,7 @@ class SearchForm(forms.Form):
         self.fields['types'] = forms.ChoiceField(label='Categary ',choices = SearchForm.get_choices())
 
 class FeedbackForm(forms.Form):
+    @staticmethod
     def get_choices():
         return [(0,'0'),(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5')]
     def __init__(self,*args,**kwargs):
