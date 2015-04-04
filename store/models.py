e2859c94fdb0dc550f6ec1ba3c7df94ff1e39bb9
linzr <noctislin@gmail.com>
2015-04-04 16:29:37 +0800

    update model to fit test database.

--- store/models.py
+++ store/models.py
@@ -4,27 +4,27 @@ from django import forms
 
 class App(models.Model):
 	GENRE_CHOICES = (
-		('G','Game'),
-		('M','Movie'),
-		('A',"App"),
-		('T',"TVShow")
+		('game','game'),
+		('movie','movie'),
+		('app',"app"),
+		('TVshow',"TVshow")
 	)
 	appid = models.PositiveIntegerField(primary_key=True)
 	name = models.CharField(max_length=32)
-	icon = models.CharField(max_length=128, null=True)
-	purchase_price = models.PositiveIntegerField()
-	rent_price = models.PositiveIntegerField(null=True)
-	description = models.CharField(max_length=256)
+	icon = models.CharField(max_length=128, blank=True, null=True)
+	purchase_price = models.FloatField()
+	rent_price = models.FloatField(blank=True, null=True)
+	description = models.CharField(max_length=1024)
 	device = models.CharField(max_length=128)
 	release_date = models.DateField()
-	genre = models.CharField(max_length=1, choices=GENRE_CHOICES)
+	genre = models.CharField(max_length=16, choices=GENRE_CHOICES)
 	def __str__(self):
           return self.name
 
 class Purchased(models.Model):
 	order_id = models.CharField(max_length=32, primary_key=True)
-	rating = models.PositiveIntegerField(null=True)
-	review = models.CharField(max_length=1024, null=True)
+	rating = models.PositiveIntegerField(blank=True, null=True)
+	review = models.CharField(max_length=1024, blank=True, null=True)
 	userid = models.ForeignKey(User)        
 	appid = models.ForeignKey(App)
 	def __str__(self):
@@ -32,8 +32,8 @@ class Purchased(models.Model):
 
 class Rent(models.Model):
 	order_id = models.CharField(max_length=32, primary_key=True)
-	rating = models.PositiveIntegerField(null=True)
-	review = models.CharField(max_length=1024, null=True)
+	rating = models.PositiveIntegerField(blank=True, null=True)
+	review = models.CharField(max_length=1024, blank=True, null=True)
 	expire_date = models.DateField()
 	userid = models.ForeignKey(User)
 	appid = models.ForeignKey(App)
